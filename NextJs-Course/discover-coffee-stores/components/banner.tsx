90092650f6b5631e9a01a98e7a654a4ab58484d3
devesh <deveshm.cs.20@nitj.ac.in>
2022-12-04 12:50:42 +0530

    coffee connoiseur

---
+++ NextJs-Course/discover-coffee-stores/components/banner.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/Banner.module.css'
+
+interface props{
+    buttonText:string
+    handleOnClick: () => void;
+}
+
+export default function Banner(props:props) {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee </span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shops</p>
+      <button className={styles.button} onClick={props.handleOnClick}>{props.buttonText}</button>
+    </div>
+  );
+}
