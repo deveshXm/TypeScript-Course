7c767573217a18ab54d752eb450b032d9cdbc745
devesh <deveshm.cs.20@nitj.ac.in>
2022-12-05 04:29:33 +0530

    coffee connoiseur

---
+++ NextJs-Course/discover-coffee-stores/pages/_document.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Link
+          rel="preload"
+          href="/fonts/IBMPlexSans-Bold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <Link
+          rel="preload"
+          href="/fonts/IBMPlexSans-Regular.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <Link
+          rel="preload"
+          href="/fonts/IBMPlexSans-SemiBold.ttf"
+          as="font"
+          crossOrigin="anonymous"
+        />
+        <Head lang="en"></Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
