3bc70485eb2ad02e77d6752d291ac24dddaaf9cc
devesh <deveshm.cs.20@nitj.ac.in>
2022-12-06 14:01:20 +0530

    coffee-store

--- NextJs-Course/discover-coffee-stores/pages/coffee-store/[id].tsx
+++ NextJs-Course/discover-coffee-stores/pages/coffee-store/[id].tsx
@@ -1,9 +1,13 @@
+import cls from "classnames";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
 import { coffeeStoreProps } from "../../types/types";
+
+import styles from "../../styles/coffee-store.module.css";
 
 export function getStaticProps({ params }: any) {
   return {
@@ -16,13 +20,13 @@ export function getStaticProps({ params }: any) {
 }
 
 export function getStaticPaths() {
-  const paths = coffeeStoresData.map(coffeeStore => {
+  const paths = coffeeStoresData.map((coffeeStore) => {
     return {
-      params:{
-        id:coffeeStore.id,
-      }
-    }
-  })
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    };
+  });
   return {
     paths,
     fallback: false, // can also be true or 'blocking'
@@ -36,17 +40,49 @@ export default function CoffeeStore(props: coffeeStoreProps) {
     return <div>Loading...</div>;
   }
 
-  const { address, name, neighbourhood } = props.coffeeStores;
+  const { address, name, neighbourhood, imgUrl } = props.coffeeStores;
+
+  const handleUpvoteButtion = () => {
+    console.log("handle upvote");
+  }
 
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href="/">Back to home</Link>
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">Back to home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <p className={styles.name}>{name}</p>
+          </div>
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          ></Image>
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" width="24" height="24" alt={address}></Image>
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" width="24" height="24" alt={neighbourhood}></Image>
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width="24" height="24" alt={"1"}></Image>
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButtion}>Up vote!</button>
+        </div>
+      </div>
     </div>
   );
 }
